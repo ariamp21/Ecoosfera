4e762d60f83d4533189a7a6a296b982d002d1fdd
ariamp21 <48859269+ariamp21@users.noreply.github.com>
2019-07-15 19:20:14 -0400

    Update Tarea_N°5.R
    Actualización de la información del proyecto

--- Tarea_N°5.R
+++ Tarea_N°5.R
@@ -37,3 +37,36 @@ for (i in paginaEcooA [2:9]){
   TituloEcoo <- gsub("\n","",TituloEcoo)
   print(TituloEcoo)
   }
+###########################################################################
+############### OBTENIENDO COMPARTIDOS DE CADA NOTICIA ######################
+##########################################################################
+
+#El orden es el siguiente
+# Nombre de la categoría
+# Links de las 10 noticias que presenta cada categoría
+# Compartidos de las noticias, respecto a Facebook, Twitter y Pinterest respectivamente
+
+
+for (i in paginaEcooA [2:9]){
+  lecturaEcoo <- read_html(i)
+  CategoriaEcoo2 <- html_text(html_nodes(lecturaEcoo,".archive-title"))
+  CategoriaEcoo2 <- gsub("\n","",CategoriaEcoo2)
+  CategoriaEcoo2 <- gsub("\t","",CategoriaEcoo2)
+ print(CategoriaEcoo2)
+  NodesNoticias <- html_nodes(lecturaEcoo, ".entry-thumbnail")
+  NodesNoticias2 <- html_nodes(NodesNoticias,"a")
+  LinksNoticias <- html_attr(NodesNoticias2, "href")
+print (LinksNoticias)
+
+for (x in LinksNoticias){
+  lecturaLinks <- read_html(x)
+  Compartidos <- html_text(html_nodes(lecturaLinks, ".sha_box"))
+  Compartidos <- gsub("Shares","",Compartidos)
+  Compartidos <- (as.numeric(Compartidos))
+print(Compartidos)
+ }
+
+}
+
+
+
