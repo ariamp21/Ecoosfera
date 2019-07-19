8d07f8c2d324a2bbd1d4b4574b7ed527d8b225be
ariamp21 <48859269+ariamp21@users.noreply.github.com>
2019-07-18 22:08:23 -0400

    Update Tarea_N°5.R
    Adjunto trabajo final.

--- Tarea_N°5.R
+++ Tarea_N°5.R
@@ -3,6 +3,7 @@
 
 #install.packages("rvest")
 library('rvest')
+library('ggplot2')
 
 #Guardando la pagina
 paginaEcoosfera <- "https://www.ecoosfera.com/"
@@ -52,17 +53,9 @@ for (i in paginaEcooA){
 
 ListaCategorias <- list()
 ListaLinks <- list()
-ListaCompartidos <- list()
-
-#Ejemplo usando DataFrame
-
+estRRSS <- data.frame()
 
 for (i in paginaEcooA){
-
-  
-  #crea dataframe
-  estadisticasRedesSociales <- data.frame()
-  
   
   lecturaEcoo <- read_html(i)
   CategoriaEcoo2 <- html_text(html_nodes(lecturaEcoo,".archive-title"))
@@ -73,79 +66,140 @@ for (i in paginaEcooA){
   NodesNoticias <- html_nodes(lecturaEcoo, ".entry-thumbnail")
   NodesNoticias2 <- html_nodes(NodesNoticias,".icon-link")
   LinksNoticias <- html_attr(NodesNoticias2, "href")
-  #print(ListaLinks)
-
+  ListaLinks <- c(ListaLinks, (LinksNoticias))
+  print(ListaLinks)
+  
   
   
   for (x in LinksNoticias){
     lecturaLinks <- read_html(x)
     Compartidos <- html_text(html_nodes(lecturaLinks, ".sha_box"))
-    Compartidos <- as.numeric(gsub("Shares","",Compartidos))
+    Compartidos <- gsub("Shares","",Compartidos)
     Compartidos <- as.numeric(Compartidos)
     
-    # se crea dataframe con los valores necesitados
-    dfTemporal <- data.frame(categoriaLink = i, Links = x, Fb = Compartidos[1], Tw = Compartidos[2], Pst = Compartidos[3])
-    dfTemporal$newColum <- (as.numeric(dfTemporal$Fb)+as.numeric(dfTemporal$Tw)+as.numeric(dfTemporal$Pst))
-
-    # Se mergean (unen) los valores en un DataFrame
-    estadisticasRedesSociales <- rbind(estadisticasRedesSociales,dfTemporal)
-    #print(Compartidos)
+    # Se crea dataframe con los valores necesitados para realizar la estadistica descriptiva
+    dfTemporal <- data.frame(Categoria = i, Fb = Compartidos[1], Tw = Compartidos[2], Pst = Compartidos[3])
+    dfTemporal$SumaCompartidos <- (as.numeric(dfTemporal$Fb)+as.numeric(dfTemporal$Tw)+as.numeric(dfTemporal$Pst))
+    
+    # Se pone la informacion dentro de un data frame creado fuera del for para no perder los datos
+    estRRSS <- rbind(estRRSS,dfTemporal)
+    print(Compartidos)
+    
   }
   print(paste("============= crea csv",CategoriaEcoo2,"============="))
-  write.csv(estadisticasRedesSociales,paste(CategoriaEcoo2,".csv",sep = ""))
+  write.csv(estRRSS,paste(CategoriaEcoo2,".csv",sep = ""))
   
 }
 
-  
-
-
-  
-  
-  
-
-
-  
-  
-  
-    
-    
-    
-    
-
-
-
-
-  
-  
-   
-   
-
-
- 
-
-
- 
 #############################################################
-############## CREANDO TABLAS ##############################
+############## CREANDO TABLAS X CATEGORIA ##################
 ############################################################
+# El orden es el siguiente
+# 1. Se creó un data frame vacio para luego meter la informacion necesaria de cada categoria
+# 2. Se realizo data frame por separado segun los 10 links por categoria que ofrece la pagina
+# 3. Se fueron adjuntando todas las tablas en un data frame final
+# 4. Luego se adjunto la tabla estRRSS la cual contiene los compartidos de cada categoria por RRSS
+# 5. Se realiza una ultima data frame para poner solo los resultados totales de los compartidos por categoria
 
-#####Estan !!malos!! los intervalos de lista compartidos, tengo q encontrar una forma de sumar de 3 en 3 los compartidos y así usar
-#####el unlist ,supongo, en la parte de val1 con [1:10]
-  
-dfTemporalArte <- data.frame(categorias = ListaCategorias[1], links= unlist(ListaLinks[1:10]), val1 = ListaCompartidos[1:3])
+dfFinal <- data.frame()
+dfArte <- data.frame(links= unlist(ListaLinks[1:10]))
+dfFinal <- rbind(dfFinal,dfArte)
 
-dfTemporalEvolucion <- data.frame(categorias = ListaCategorias[2], links= unlist(ListaLinks[11:20]), val1 = ListaCompartidos[31:33])
+dfEvolucion <- data.frame(links= unlist(ListaLinks[11:20]))
+dfFinal <- rbind(dfFinal,dfEvolucion)
 
-dfTemporalGuia <- data.frame(categorias = ListaCategorias[3], links= unlist(ListaLinks[21:30]), val1 = ListaCompartidos[61:63])
+dfGuia <- data.frame(links= unlist(ListaLinks[21:30]))
+dfFinal <- rbind(dfFinal,dfGuia)
 
-dfTemporalMA <- data.frame(categorias = ListaCategorias[4], links= unlist(ListaLinks[31:40]), val1 = ListaCompartidos[91:93])
+dfMA <- data.frame(links= unlist(ListaLinks[31:40]))
+dfFinal <- rbind(dfFinal,dfMA)
 
-dfTemporalNoticias <- data.frame(categorias = ListaCategorias[5], links= unlist(ListaLinks[41:50]), val1 = ListaCompartidos[121:123])
+dfNoticias <- data.frame(links= unlist(ListaLinks[41:50]))
+dfFinal <- rbind(dfFinal,dfNoticias)
 
-dfTemporalSci <- data.frame(categorias = ListaCategorias[6], links= unlist(ListaLinks[51:60]), val1 = ListaCompartidos[151:153])
+dfSci <- data.frame(links= unlist(ListaLinks[51:60]))
+dfFinal <- rbind(dfFinal,dfSci)
 
-dfTemporalWell <- data.frame(categorias = ListaCategorias[7], links= unlist(ListaLinks[61:70]), val1 = ListaCompartidos[181:183])
+dfWell <- data.frame(links= unlist(ListaLinks[61:70]))
+dfFinal <- rbind(dfFinal,dfWell)
 
-dfTemporalViajes <- data.frame(categorias = ListaCategorias[8], links= unlist(ListaLinks[71:80]), val1 = ListaCompartidos[211:213])
+dfViajes <- data.frame(links= unlist(ListaLinks[71:80]))
+dfFinal <- rbind(dfFinal,dfViajes)
 
-dfTemporalCol <- data.frame(categorias = ListaCategorias[9], links= unlist(ListaLinks[81:90]), val1 = ListaCompartidos[241:243])
+dfCol <- data.frame(links= unlist(ListaLinks[81:90]))
+dfFinal <- rbind(dfFinal,dfCol)
+
+dfFinal <- cbind(dfFinal,estRRSS)
+
+write.csv(dfFinal, file = "TablaFinalCompartidos.csv")
+
+
+############################################################
+############## ESTADISTICA DESCRIPTIVA ####################
+##########################################################
+
+summary(dfFinal)
+#       Fb                Tw               Pst           SumaCompartidos   
+#Min.   :   12.0   Min.   :    9.0   Min.   :     0.0   Min.   :   146.0  
+#1st Qu.:  262.2   1st Qu.:   24.0   1st Qu.:   242.5   1st Qu.:   977.8  
+#Median :  680.0   Median :  269.0   Median :   337.5   Median :  3238.5  
+#Mean   : 2537.7   Mean   : 2829.5   Mean   :  4858.7   Mean   : 10225.9  
+#3rd Qu.: 1896.0   3rd Qu.:  843.2   3rd Qu.:  2674.8   3rd Qu.:  9068.5  
+#Max.   :27493.0   Max.   :36421.0   Max.   :188644.0   Max.   :192203.0  
+
+
+#Desglose de los datos
+#En Facebook la noticia que menos compartidos tuvo fue https://ecoosfera.com/arte-consciente-denuncia-la-emergencia-climatica-olafur-eliasson-in-real-life/
+#la cual estaba publicada en dos categorias "Arte y Guia Gaia". Por otro lado la que más compartidos tuvo fue 
+#https://ecoosfera.com/queen-brian-may-tesis-de-doctorado-astrofisico/ publicada en la categoria "Sci-Innovacion".
+
+#En twitter la noticia con menos compartidos fue https://ecoosfera.com/ecoturismo-mexico-guia-donde-ir-mejores-destinos/
+#parte de "Wellness". La con mayor cantidad de compartidos fue https://ecoosfera.com/ultima-luna-llena-luna-fria-planes-disfrutar-cielo/
+#de la categoria "Viajes"
+
+#En Pinterest la noticia que nunca fue compartida es https://ecoosfera.com/ecoturismo-mexico-guia-donde-ir-mejores-destinos/
+#de "Viajes". La mas compartida fue https://ecoosfera.com/amazonas-deforestacion-pierde-arboles-campo-futbol-bolsonaro/
+#de "Medio Ambiente".
+
+#Podemos notar, que la noticia respecto a ecoturismo en la seccion "Viajes" y "Wellness" serian las que menos importancia le da el publico
+#debido a que coincide la misma noticia en Twitter y Pinterest como la menos compartida.
+#Ademas podemos notar que los roles de estas RRSS se pueden demostrar, por ejemplo en Pinterest la mas compartida
+#es respecto a la deforestacion en "Medio Ambiente", esto sigue la corriente de su rol de demostrar realidad en fotos.
+#En Twitter el publico suele visitar aquello que es "trending topics", lo cual fue hace un par de dias la luna llena en la
+#tarde y noche del 17 de julio.
+#Y finalmente en Facebook donde su publico actualmente lo utiliza para ver videos o aprender cosas rapidamente, las 
+#cuales en epocas anteriores no se obtenian con tal velocidad.
+
+
+#En cuanto a la Suma total de compartidos por noticia, la que menor numero de compartidos tuvo fue 
+#https://ecoosfera.com/caribe-mexicano-emergencia-contaminacion-corales-blanqueamiento/ y la mas compartida fue
+#https://ecoosfera.com/amazonas-deforestacion-pierde-arboles-campo-futbol-bolsonaro/. La primera publicada en
+#"Columna especial" y la segunda en "Medio Ambiente".
+
+#Podemos decir que la media de los compartidos para Facebook es de 2537 aproximadamente, para Twitter es de 2829 aproximadamente
+#y de 4858 para Pinterest. Si nos guiamos por estos datos, podemos decir que la aplicacion que mas se informa mediante
+#Ecoosfera son los usuarios de "Pinterest".
+
+#A modo general podemos decir que el minimo esperado de compartidos para una noticia es de 146 en "Columna Especial" y el maximo es de 
+#192 mil aproximadamente en "Medio Ambiente".
+
+#################################################################
+##################### GRAFICOS ##################################
+##################################################################
+
+hist(dfFinal$Fb, main="Noticias compartidas en Facebook", ylab="Frecuencia en links",xlab="Compartidos")
+
+#Podemos notar que casi 80 de los links de Facebook se comparten generalmente hasta 5000 veces.
+
+hist(dfFinal$Tw, main="Noticias compartidas en Twitter", ylab="Frecuencia en links",xlab="Compartidos")
+
+#Se aprecia que generalmente hay 5000 compartidos en casi la totalidad de los links, al igual que facebook.
+
+hist(dfFinal$Pst, main="Noticias compartidas en Pinterest", ylab="Frecuencia en links",xlab="Compartidos")
+#La cantidad que generalmente se comparte son de 2500 salvo por algunas que ocasionan revuelo y llegan hasta
+#200.000 compartidos
+
+
+
+########FIN########
+#PD: No hay tildes debido a que despues salen cuadrados extraños que me perturban.
